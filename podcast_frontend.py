5f44fdbda458180161bdd720a0fdf6987a11a50d
Andy Ennamorato <andy@ninjacat.io>
2023-08-24 00:28:30 -0600

    live podcast pulls were failing
    * I did not implement pulling more details on the guest or host, so live processing fails.
      let's just move along...

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -83,18 +83,20 @@ def main():
             st.write(podcast_info['podcast_summary'])
 
         with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+            st.image(podcast_info['podcast_details']['episode_image'],
+                     caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
         col3, col4 = st.columns([3, 7])
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            st.write(podcast_info['podcast_guest'])
 
+        # not implemented :(
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            st.write("sorry, not implemented")
 
         # Display the five key moments
         st.subheader("Key Moments")
